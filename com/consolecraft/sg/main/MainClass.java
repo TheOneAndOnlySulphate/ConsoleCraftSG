de17ebda6e6d3065d13158fe270d09fa6e2a410b
TheRandomGamerMC <robin.devos1@gmail.com>
2015-06-22 20:43:02 +0200

    Update MainClass.java

--- com/consolecraft/sg/main/MainClass.java
+++ com/consolecraft/sg/main/MainClass.java
@@ -1,1 +1,19 @@
-//This is the new repo! :) 
+package com.consolecraft.sg.main;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public class main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new mapChoose(), this);
+    saveDefaultConfig();
+
+        }
+
+
+}
