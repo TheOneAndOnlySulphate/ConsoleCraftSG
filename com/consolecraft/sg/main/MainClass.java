c99bb1816cabb10799b00768fecc98a537156884
TheOneAndOnlySulphate <windowsxpprofailiure@gmail.com>
2015-06-23 17:54:12 +0100

    Added get() method, cleaned up a bit

--- com/consolecraft/sg/main/MainClass.java
+++ com/consolecraft/sg/main/MainClass.java
@@ -1,20 +1,26 @@
 package com.consolecraft.sg.main;
 
-
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
-
 public class main extends JavaPlugin {
+    
+    private static MainClass plugin;
 
     @Override
     public void onEnable() {
+        plugin = this;
        Bukkit.getPluginManager().registerEvents(new mapChoose(), this);
     saveDefaultConfig();
     }
     
     @Override
     public void onDisable() {
+        plugin = null;
     }
+    
+    public MainClass get() {
+        return plugin;
+    }
+    
 }
