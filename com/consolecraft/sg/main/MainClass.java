baacd0100402569fa27b1a44086a8a9d39f693fd
TheOneAndOnlySulphate <windowsxpprofailiure@gmail.com>
2015-06-23 17:58:41 +0100

    Registered Countdown, renamed 'class'

--- com/consolecraft/sg/main/MainClass.java
+++ com/consolecraft/sg/main/MainClass.java
@@ -1,17 +1,19 @@
 package com.consolecraft.sg.main;
 
 import org.bukkit.Bukkit;
+import com.consolecraft.sg.schedulers.Countdown;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class main extends JavaPlugin {
+public class MainClass extends JavaPlugin {
     
     private static MainClass plugin;
 
     @Override
     public void onEnable() {
         plugin = this;
-       Bukkit.getPluginManager().registerEvents(new mapChoose(), this);
-    saveDefaultConfig();
+        Bukkit.getPluginManager().registerEvents(new mapChoose(), this);
+        Bukkit.getPluginManager().registerEvents(new Countdown(), this);
+        saveDefaultConfig();
     }
     
     @Override
