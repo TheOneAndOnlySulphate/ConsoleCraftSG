36e73ebe268088816afc753f1a931fff00f5fca4
TheOneAndOnlySulphate <windowsxpprofailiure@gmail.com>
2015-06-24 20:18:36 +0100

    Added values class

---
+++ com/consolecraft/sg/misc/Values.java
@@ -0,0 +1,19 @@
+package com.consolecraft.sg.misc;
+
+public class Values {
+
+    //Put the hashmaps in here
+    
+    public HashMap getTier1() {
+        return //the hashmap for tier 1 items
+    }
+    
+    public HashMap getTier2() {
+        return //the hashmap for tier 2 items
+    }
+    
+    public HashMap getTier3() {
+        return //the hashmap for tier 3 items
+    }
+
+}
