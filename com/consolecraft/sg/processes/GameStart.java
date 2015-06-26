3517b51e4d4bfd20623eccf6945b037120de249a
TheOneAndOnlySulphate <windowsxpprofailiure@gmail.com>
2015-06-26 12:36:15 +0100

    Started to fully create the GameStart.java class
    Added player teleportation method, and methods to get the spawns.

--- com/consolecraft/sg/processes/GameStart.java
+++ com/consolecraft/sg/processes/GameStart.java
@@ -1,7 +1,58 @@
 package com.consolecraft.sg.processes;
 
+//TODO: Add imports!
+
 public class GameStart {
     
+    BukkitUtils b = new BukkitUtils();
     
+    public Location getSpawn1() {
+        World w = Bukkit.getWorld(MainClass.get().getConfig().getString("world");
+        double x = MainClass.get().getConfig().getDouble("spawn1x");
+        double y = MainClass.get().getConfig().getDouble("spawn1y");
+        double z = MainClass.get().getConfig().getDouble("spawn1z");
+        Location spawn1 = new Location(w,x,y,z);
+        return spawn1;
+    }
     
+    public Location getSpawn2() {
+        World w = Bukkit.getWorld(MainClass.get().getConfig().getString("world");
+        double x = MainClass.get().getConfig().getDouble("spawn2x");
+        double y = MainClass.get().getConfig().getDouble("spawn2y");
+        double z = MainClass.get().getConfig().getDouble("spawn2z");
+        Location spawn2 = new Location(w,x,y,z);
+        return spawn2;
+    }
+    
+    public Location getSpawn3() {
+        World w = Bukkit.getWorld(MainClass.get().getConfig().getString("world");
+        double x = MainClass.get().getConfig().getDouble("spawn3x");
+        double y = MainClass.get().getConfig().getDouble("spawn3y");
+        double z = MainClass.get().getConfig().getDouble("spawn3z");
+        Location spawn3 = new Location(w,x,y,z);
+        return spawn3;
+    }
+    
+    public void teleportPlayers() {
+        int spawn = 1;
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            if (spawn == 1) {
+                p.teleport(spawn1);
+                spawn++;
+                continue;
+            }
+            else if (spawn == 2) {
+                p.teleport(spawn2);
+                spawn++;
+                continue;
+            }
+            else if (spawn == 3) {
+                p.teleport(spawn3);
+                spawn++
+            }
+            else {
+                p.teleport(spawn1);
+            }
+        }
+    }
 }
